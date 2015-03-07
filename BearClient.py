57222c6327344ae7058aa4060ed101111abec831
lanselambor <757016515@qq.com>
2015-03-07 16:08:01 +0000

    update

--- BearClient.py
+++ BearClient.py
@@ -3,35 +3,44 @@
 import socket
 import time
 import grovepi
+import select
 
 input = 2
 count = 0
 # Connect the speaker to digital port D3
 speaker = 3
 grovepi.pinMode(speaker,"OUTPUT")
+#grovepi.pinMode(input,"INPUT")
 time.sleep(1)
 i = 0
 
 address = ('192.168.21.205', 31500)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # s = socket.socket()
-s.connet(address)
+s.connect(address)
 
 while True:
     try:
-        #Socket recieve 
-        data = s.recv(4)
-        print 'the data received is',data
+        #Not blocked socket
+        infds, outfds, errfds = select.select([s,],[],[],1)
+        #if infds changed, get to process, ignore if not changed
+        if len(infds) != 0:
+            buf = s.recv(4)
+            if len(buf) != 0:
+                print 'rev:', buf
+                if buf == 'bear':
+                    grovepi.digitalWrite(speaker,1)
+                    time.sleep(3)
+            print 'no data coming'
 
-        if data == "bear":
-            grovepi.digitalWrite(speaker,1)
-            time.sleep(4)  # wait for bear speaking
-        
         # Read resistance from Potentiometer
-        i = grovepi.analogRead(input)
+        i = grovepi.analogRead(input)      
         if i >= 1020:
+            time.sleep(0.05)
+#        if grovepi.digitalRead(input) == 1: 
             time.sleep(0.05)
             i = grovepi.analogRead(input)
             if i >= 1020:                    
+#            if grovepi.digitalRead(input) == 1:
                 count += 1
                 if count == 100:
                     count = 0
@@ -41,7 +50,7 @@ while True:
                 grovepi.digitalWrite(speaker,1)
                 #socket send
                 s.send('bear')
-                time.sleep(4)
+                time.sleep(3)
         else:
             #speaker close
             grovepi.digitalWrite(speaker,0)
--- BearServer.py
+++ BearServer.py
@@ -3,12 +3,14 @@
 import socket
 import time
 import grovepi
+import select
 
 input = 2
 count = 0
 # Connect the speaker to digital port D3
 speaker = 3
 grovepi.pinMode(speaker,"OUTPUT")
+grovepi.pinMode(input,"INPUT")
 time.sleep(1)
 i = 0
 
@@ -16,24 +18,35 @@ address = ('192.168.21.205', 31500)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # s = socket.socket()
 s.bind(address)
 s.listen(5)
-
-ss, addr = s.accept()
-
+#ss, addr = s.accept()
 
 
 while True:
     try:
-        #Socket recieve 
-        ra = ss.recv(4)
-        if ra == 'bear':
-            grovepi.digitalWrite(speaker,1)
-       
+        #Not blocked socket
+        infds, outfds, errfds = select.select([s,],[],[],1)
+        #if infds changed, get to process, ignore if not changed
+        if len(infds) != 0:
+            ss, addr = s.accept()
+            infds_c, outfds_c, errfds_c = select.select([ss],[],[],3)
+            if len(infds_c) != 0:
+                buf = ss.recv(4)
+                if len(buf) != 0:
+                    print buf
+                    if buf == 'bear':
+                        grovepi.digitalWrite(speaker,1)
+                        time.sleep(3)
+                    ss.close()    
+            print 'no data coming'    
+
         # Read resistance from Potentiometer
         i = grovepi.analogRead(input)
         if i >= 1020:
+#        if grovepi.digitalRead(input) == 1:
             time.sleep(0.05)
             i = grovepi.analogRead(input)
             if i >= 1020:                    
+#            if grovepi.digitalRead(input) == 1:
                 count += 1
                 if count == 100:
                     count = 0
@@ -42,7 +55,8 @@ while True:
                 #speaker open
                 grovepi.digitalWrite(speaker,1)
                 #socket send
-               ss.send('bear')
+                ss.send('bear')
+                time.sleep(4)
         else:
             #speaker close
             grovepi.digitalWrite(speaker,0)
